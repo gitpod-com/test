4b4679e9c7214a8f2340e45c15a8c489a01f9003
Jean Pierre <jeanp413@hotmail.com>
2022-07-26 17:54:34 +0000

    test

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('@types/eslint').Linter.BaseConfig}
+ */
+module.exports = {
+  extends: [
+    "plugin:mdx/recommended",
+  ],
+  // we're using vitest which has a very similar API to jest
+  // (so the linting plugins work nicely), but it means we have to explicitly
+  // set the jest version.
+  settings: {
+  },
+}
